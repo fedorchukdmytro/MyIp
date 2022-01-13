89ed5c527595124445e03fd9f0ba382d1333cea5
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2022-01-13 21:24:33 +0000

    11

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import os
 
 
 PASSWORD = os.environ['PASSWORD']
+USERNAME = os.environ['USERNAME']
 
 parameters ={}
 
@@ -21,18 +22,19 @@ class tc_one(aetest.Testcase):
         # server = subprocess.Popen("ssh {user}@{host} {cmd}".format(user='dmytrofedorchuk', host='172.22.35.18', cmd='-s'))     #, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname='172.22.35.18' ,username='dmytrofedorchuk',password=PASSWORD)
+        ssh_client.connect(hostname='192.168.1.136' ,username=USERNAME,password=PASSWORD)
         parameters.update({'ssh_client': ssh_client })
         # stdin, stdout, sterr = ssh_client.exec_command('/opt/homebrew/bin/iperf3 -s -1')
 
     @aetest.test
-    def client_launching(self, ssh_client):
+    def server_launching(self, ssh_client):
         stdin, stdout, sterr = ssh_client.exec_command('/opt/homebrew/bin/iperf3 -s -1')
         time.sleep(60)
        # remote_connection.send("iperf3 -s") 
         # remote_connection.send("\n") 
         
     @aetest.cleanup
+
     def clean_testcase(self, ssh_client):
         logger.info("Pass testcase cleanup")
         ssh_client.close()
