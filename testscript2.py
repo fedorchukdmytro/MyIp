8d29c9063443bb66c545c5a458007b1c9e191f28
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2022-02-14 16:42:28 +0000

    1

--- testscript2.py
+++ testscript2.py
@@ -35,24 +35,30 @@ class CommonSetup(aetest.CommonSetup):
         parameters.update({'serv_sterr': serv_sterr })
         sftp = ssh_client.open_sftp()
         parameters.update({'sftp': sftp})
-    
+
     @aetest.subsection
-    def common_setup_params_client(self, IPAddress, util, flag1, flag2, flag3, flag4 ):
+    def common_setup_params_client(self, IPAddress, util, flag1, flag2, flag3, flag4, flag5 ):
         logger.info('>>>>>>>>>>>>>>>>>>>>>>>>Lounching iperf client with subprocess<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
-        client_process = subprocess.Popen([util, flag1, IPAddress, flag2, flag3, flag4], stdout=PIPE, stderr=PIPE)
+        client_process = subprocess.Popen([util, flag5, flag1, IPAddress, flag2, flag3, flag4], stdout=PIPE, stderr=PIPE)
         client_process.wait()
         parameters.update({'client_process': client_process })    
-      
     
+   
     @aetest.subsection
-    def common_telnet(self, t):
+    def telnet_connection_setup(self, t):
         logger.info('>>>>>>>>>>>>>>>>>>>>>>>>TELNET<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
         ip = '10.0.0.3'
         t.connect(ip, username='root', password='danya', p=23, timeout=1)
-        output1 = t.execute('timeout -t 10 tcpdump -i br0 -nn host 10.0.0.10 and udp -w tcpdump_out.pcap')
-        print(output1)  
+        output1 = t.execute('timeout -t 10 tcpdump -i br0 -nn -w tcpdump_out.pcap')
+        print(output1)   
+
+
+
 
 class MyTestcase(aetest.Testcase):
+
+  
+       
 
     @aetest.test
     def verifying_server_stdout(self, serv_stdout):
@@ -63,23 +69,21 @@ class MyTestcase(aetest.Testcase):
     def verifying_server_sterr(self, serv_sterr):
         logger.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>server standart error<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
         assert serv_sterr.readlines() == []
-        print(serv_sterr.readlines)
         
     @aetest.test
     def verifying_client_return_code(self, client_process):
         logger.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>client return code<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
         assert client_process.returncode == 0
-
+        
+     
     @aetest.test
-    def second_control_speed_test(self, client_process):   
+    def control_speed_test(self, client_process ):
         logger.info('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>assertion speed test<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
         data =json.loads(client_process.stdout.read().decode('ascii').strip("\n"))
-        assert (data['end']['streams'][0]['receiver']['bytes'] / 1000000) > 20
-        assert (data['end']['streams'][0]['receiver']['bits_per_second'] / 1000000) > 5
-        print(data)
-        t.execute('tftp -pl tcpdump_outTCP.pcap 10.0.0.1')
-    
-    
+        assert (data['end']['streams'][0]['udp']['bytes'] / 1000) > 10
+        assert (data['end']['streams'][0]['udp']['bits_per_second'] / 1000) > 10
+        # print(data)
+        t.execute('tftp -pl tcpdump_out.pcap 10.0.0.1')
 
     @aetest.test
     def verifying_client_standart_error(self, client_process):
@@ -100,14 +104,15 @@ class MyTestcase(aetest.Testcase):
     #     print(sys.getsizeof('tcpdump_out.pcap'))
     #     assert sys.getsizeof('tcpdump_out.pcap') > 60
 
-
+   
+   
 
 class ScriptCommonCleanup(aetest.CommonCleanup):
     @aetest.subsection
     def disconnect_from_devices(self, ssh_client, t, sftp):
         logger.info("Pass testcase cleanup")
-        t.execute('rm tcpdump_outTCP.pcap')
         
+        t.execute('rm tcpdump_out.pcap')
         sftp.close()
         ssh_client.close()
         t.close()
